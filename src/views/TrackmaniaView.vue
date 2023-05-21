1b6eb5d6dc23e626cdb0d9144f99be2fd7fac907
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2023-05-21 22:28:08 +0200

    Added showcase and background change delay

--- src/views/TrackmaniaView.vue
+++ src/views/TrackmaniaView.vue
@@ -1,4 +1,6 @@
 <script>
+import ShowcaseList from '../components/ShowcaseList.vue';
+
 export default {
     props: {
         isDarkMode: {
@@ -7,6 +9,7 @@ export default {
     },
     data() {
         return {
+            data: SITE_DATA, // eslint-disable-line no-undef
             mapBackground: null,
             mapBackgroundFallback: {
                 "mxID": 36270,
@@ -15,33 +18,57 @@ export default {
                 "author": "Sláys & htimh"
             },
             backgroundAlpha: 0.65,
+            bgColorModeInterval: null,
+            bgIntervalDelay: 7000,
+            bgIntervalTimer: 0,
+            bgIntervalDivWidth: 0,
             bgInterval: null,
-        }
+            bgIntervalDivDirectionFw: true
+        };
     },
     mounted() {
-        if (window.fetch) this.fetchBackgroundImage();
-        else this.setBackgroundImage();
-        this.bgInterval = setInterval(()=>{
+        if (window.fetch)
+            this.fetchBackgroundImage();
+        else
+            this.setBackgroundImage();
+        this.bgColorModeInterval = setInterval(() => {
             this.backgroundAlpha = this.isDarkMode ? 0.65 : 0;
-            document.querySelector("div#app>canvas").style.backgroundColor = "rgba(0, 0, 0, "+this.backgroundAlpha+")";
+            document.querySelector("div#app>canvas").style.backgroundColor = "rgba(0, 0, 0, " + this.backgroundAlpha + ")";
         }, 0);
+        this.bgInterval = setInterval(this.backgroundImageInterval, 0);
     },
     methods: {
         async fetchBackgroundImage() {
+            this.bgIntervalTimer = 0;
+            this.bgIntervalDivDirectionFw = !this.bgIntervalDivDirectionFw;
             let res = await fetch("/api/trackmaniabackgrounds.json");
             if (!res.ok) {
                 console.error("Error while fecthing backgrounds from API:", res.status, res.statusText);
                 return this.setBackgroundImage();
             }
-            let json = await res.json(),
-                game = Object.keys(json),
-                selectedGame = this.randomItem(game);
-            this.mapBackground = this.randomItem(json[selectedGame]);
+            let json = await res.json(), game = Object.keys(json), selectedGame = this.randomItem(game), selectedMap = this.randomItem(json[selectedGame]);
+            while (this.mapBackground != null && selectedMap.mxID == this.mapBackground.mxID)
+                selectedMap = this.randomItem(json[selectedGame]); // not taking the same map
+            this.mapBackground = selectedMap;
             this.setBackgroundImage(this.mapBackground);
         },
+        backgroundImageInterval() {
+            if (this.bgIntervalTimer < this.bgIntervalDelay) {
+                this.bgIntervalTimer += 1;
+                let progressPercent = (this.bgIntervalTimer / this.bgIntervalDelay) * 100;
+                if (!this.bgIntervalDivDirectionFw)
+                    this.bgIntervalDivWidth = Math.ceil((100 - progressPercent) / 10);
+                else
+                    this.bgIntervalDivWidth = Math.floor(progressPercent / 10);
+                if (document.querySelector("div.progress-circle") != null)
+                    document.querySelector("div.progress-circle").style.borderWidth = this.bgIntervalDivWidth + "px";
+            }
+            else
+                this.fetchBackgroundImage();
+        },
         setBackgroundImage(map = this.mapBackgroundFallback) {
-            document.querySelector("div#app>canvas").style.background = "url("+map.url+")";
-            document.querySelector("div#app>canvas").style.backgroundColor = "rgba(0, 0, 0, "+this.backgroundAlpha+")";
+            document.querySelector("div#app>canvas").style.background = "url(" + map.url + ")";
+            document.querySelector("div#app>canvas").style.backgroundColor = "rgba(0, 0, 0, " + this.backgroundAlpha + ")";
             document.querySelector("div#app>canvas").style.backgroundSize = "cover";
             document.querySelector("div#app>canvas").style.backgroundBlendMode = "darken";
         },
@@ -51,19 +78,43 @@ export default {
             document.querySelector("div#app>canvas").style.backgroundSize = "";
             document.querySelector("div#app>canvas").style.backgroundBlendMode = "";
         },
-        randomItem(array){
+        randomItem(array) {
             return array[Math.floor(Math.random() * array.length)];
         }
     },
     unmounted() {
+        clearInterval(this.bgColorModeInterval);
         clearInterval(this.bgInterval);
         this.removeBackgroundImage();
-    }
+    },
+    components: { ShowcaseList }
 }
 </script>
 
 <template>
     <div>
-
+        <ShowcaseList :showcases="data.trackmaniaShowcase" />
+    </div>
+    <div class="bgImage-desc is-size-6 has-text-centered" v-if="mapBackground != null">
+        <i class="fa fa-map" aria-hidden="true" />&thinsp;
+        {{ mapBackground.name }} by {{ mapBackground.author }}
+        <div class="progress-circle" @click="fetchBackgroundImage" />
     </div>
 </template>
+
+<style scoped>
+.bgImage-desc {
+    margin-bottom: -1em;
+}
+
+.progress-circle{
+    float: right;
+    box-sizing: border-box;
+    font-size: 1.25em;
+    width: 1em;
+    height: 1em;
+    border-radius: 50%;
+    border: 2px solid;
+    border-width: 0px;
+}
+</style>
