3607d10bb070f0ec1f026c2efffaf3e569c052ee
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2023-05-21 22:28:19 +0200

    Target blank if link is external

--- src/components/ShowcaseList.vue
+++ src/components/ShowcaseList.vue
@@ -11,13 +11,13 @@ export default {
 <template>
     <div class="columns" v-for="showcase in showcases" v-bind:key="showcase.name">
         <div class="column is-one-third" v-if="showcase.image">
-            <a :href="showcase.link">
+            <a :href="showcase.link" :target="showcase.link.startsWith('http') ? '_blank' : ''">
                 <img :alt="showcase.name" :src="'/img/'+showcase.image" width="200">
             </a>
         </div>
         <div class="column">
             <h3 class="is-size-4 pb-3">
-                <a :href="showcase.link">{{ showcase.name }}</a>
+                <a :href="showcase.link" :target="showcase.link.startsWith('http') ? '_blank' : ''">{{ showcase.name }}</a>
             </h3>
             <p v-if="showcase.description">{{ showcase.description }}</p>
         </div>
