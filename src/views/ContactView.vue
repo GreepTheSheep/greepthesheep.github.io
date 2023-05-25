2bbc9b3dd2547b69a0781169c7a8da9390bbe22f
Matthieu <89matt89.md@gmail.com>
2023-05-25 10:24:30 +0200

    Added contacts

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+            mail: "greep@greep.fr"
+        }
+    }
+}
+</script>
+
 <template>
-    <div>
-        Soon...
+    <div class="block is-size-3 has-text-centered">
+        <a :href="'mailto:'+mail">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+            &nbsp;
+            <span>{{ mail }}</span>
+        </a>
+    </div>
+    <div class="block is-size-3 has-text-centered">
+        <router-link to="/discord">
+            <i class="fa fa-discord-alt" aria-hidden="true"></i>
+            &nbsp;
+            <span>Discord server</span>
+        </router-link>
     </div>
 </template>
